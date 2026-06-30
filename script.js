0ff2e727f378aee012e0aff978e4e82e1e0160cc
anukumari887 <naveeti6223anu@gmail.com>
2026-06-30 23:54:20 +0530

    Updated countdown website

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 let num = document.querySelector(".circle2");
-
-let stop;
+let pause =  document.querySelector("#pause");
+let play = document.querySelector("#play");
+let reset = document.querySelector("#reset");
 let count = 10;
+let stop;
 
-stop = setInterval(() => {
+
+
+play.addEventListener("click", () => {
+
+     stop = setInterval(() => {
     num.innerHTML = count;
     console.log(count);
     count--;
+     num.style.fontSize = "190px";
 
-    if(count == -1){
-        
+    if(count < -1){
         clearInterval(stop);
+         stop = null;
         num.innerHTML = "Time's Up"
         console.log("Time's up");
         num.style.fontSize = "60px";
     }
+
 }, 1000);
+})
+
+ pause.addEventListener("click", () => {
+        clearInterval(stop);
+        stop = null;
+    })
+
+        reset.addEventListener("click", () => {
+            clearInterval(stop);
+             stop = null;
+            count = 10;
+            num.innerHTML = count;
+            num.style.fontSize = "190px";
+})
+
+
+
+
+
